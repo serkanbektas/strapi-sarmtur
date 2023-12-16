59e329998d0d8a6b2c7a2e53e6649c5820ee6082
Serkan Bektaş <quisif@gmail.com>
2023-12-16 21:49:43 +0300

    main Apollo updated

--- config/plugins.js
+++ config/plugins.js
@@ -31,7 +31,8 @@ module.exports = ({ env }) => ({
     // enables the plugin only in development mode
     // if you also want to use it in production, set this to true
     // keep in mind that graphql playground has to be enabled
-    enabled: process.env.NODE_ENV === "production" ? false : true,
+    // enabled: process.env.NODE_ENV === "production" ? false : true,
+    enabled: true,
     // endpoint: "https://tunneled-strapi.com/graphql", // OPTIONAL - endpoint has to be accessible from the browser
   },
 });
